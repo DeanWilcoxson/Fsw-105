097c6ccd436a70c30d3fd34286b3409065dc95a4
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-10-22 19:46:47 -0700

    10/22/20

--- week1/index.js
+++ week1/index.js
@@ -1,14 +1,14 @@
 var shopperObject = {
+    isMale: true,
+    age: 29,
     firstName: "Dean",
     lastName: "Wilcoxson",
     preferredNickName: "Deanoe",
-    age: 29,
     birthDate: "07/28/1991",
     driversLicenseNumber: "E3874612",
     groceryList: ["Apples", "Steak", "Milk", "Bread", "Cheddar_Cheese", ],
     fullName: function() {
         return this.firstName + " " + this.lastName;
-    },
-    isMale: true
+    }
 };
-console.log(shopperObject);
+console.log(shopperObject.preferredNickName);
