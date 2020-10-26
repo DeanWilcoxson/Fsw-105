7fefc85964c86aa5617435f32b100866584677ca
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-10-26 03:15:25 -0700

    10/26/20.4

--- week2/if_statement_olympics/index.js
+++ week2/if_statement_olympics/index.js
@@ -27,40 +27,51 @@ if (1 === 1) { console.log("strict"); } else if (1 == "1") { console.log("loose"
 if ((1 <= 2) && (2 === 4)) { console.log("yes"); }
 
 //--------------------------Gold Medal (Extra Credit)----------------------------
-//-----Number1 (Replaces above if statements with ternary's.)
 
-//--Preliminary Number1
+//-----Number1 (Replace above If-statements with Ternary's.)
+
+//Preliminary Number1
 5 >= 3 ? ($Result = "is greater than") : ($Result = "is not greater than");
 console.log($Result);
 
-//--Preliminary Number2
+//Preliminary Number2
 //This if-statement reacts differently when in ternary format. Is that correct?
 "cat" === 3 ? ($Result = "is the length") : ($Result = "is not the length");
 console.log($Result);
 
-//--Preliminary Number3
+//Preliminary Number3
 "cat" === "dog" ? ($Result = "the same") : ($Result = "not the same");
 console.log($Result);
 
-//--Bronze Number1
+//Bronze Number1
 var person = { name: "Bobby", age: 12 };
 "Bobby".age === 18 ? ($Result = "allowed") : ($Result = "underage");
 console.log($Result);
 
-//--Bronze Number2
+//Bronze Number2
 "Bobby".charAt(0) === "B" ? ($Result = "allowed") : ($Result = "Invalid");
 console.log($Result);
 
-//--Bronze Number3
+//Bronze Number3
 "Bobby".charAt(0) === "B" && "Bobby".age === 18 ? ($Result = "allowed") : ($Result = "not allowed");
 console.log($Result);
 
-//--Silver Number1
+//Silver Number1
 1 === 1 || 1 == "1" ? $Result = "strict" || "loose" : $Result = "not equal at all";
 console.log($Result);
 
-//--Silver Number2
+//Silver Number2
 1 <= 2 && 2 === 4 ? $Result = "yes" : $Result = "no";
 console.log($Result);
 
-//-----Number2 (Write an if statement that checks to see if "dog" is a string.)
+//-----Number2 (Write an If-statement that checks to see if "dog" is a string.)
+
+//-----Number3 (Write an If-statement that checks to see if "true" is a boolean.)
+
+//-----Number4 (Write an If-statement that checks to see if a variable has been defined or not.)
+
+//-----Number5 (Write an If-statement asking if "s" greater than 12?)
+
+//-----Number6 (Try it with a few more letters and a few numbers.)
+
+//-----Number7 (Write a Ternary statement that console.logs whether a number is odd or even.)
