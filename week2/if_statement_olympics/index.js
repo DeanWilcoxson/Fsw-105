4eb89bddcab30f5d6c923cff01d7a0e6e13ffe5f
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-10-26 01:22:48 -0700

    10/26/20.1

--- week2/if_statement_olympics/index.js
+++ week2/if_statement_olympics/index.js
@@ -1,4 +1,4 @@
-// Preliminaries
+//Preliminaries
 if (5 >= 3); { console.log("is greater than"); }
 if ("cat" === 3); { console.log("is the length"); }
 if ("cat" === "dog") { console.log("the same"); } else if ("cat" !== "dog") { console.log("not the same"); }
