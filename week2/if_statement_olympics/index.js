f95be0a98bb407054f81f3ca85ba8e92fdf4e4c5
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-10-26 03:04:32 -0700

    10/26/20.3

--- week2/if_statement_olympics/index.js
+++ week2/if_statement_olympics/index.js
@@ -10,7 +10,7 @@ if ("cat" === "dog") { console.log("the same"); } else if ("cat" !== "dog") { co
 
 //--------------------------Bronze medal-----------------------------------------
 //Number1
-var person = { name: "Bobby", age: 12 };
+var person = { name: "Bobby", age: 12, };
 if ("Bobby".age === 18) { console.log("allowed"); } else if ("Bobby".age !== 18) { console.log("underage"); }
 
 //Number2
@@ -26,22 +26,41 @@ if (1 === 1) { console.log("strict"); } else if (1 == "1") { console.log("loose"
 //Number2
 if ((1 <= 2) && (2 === 4)) { console.log("yes"); }
 
-//Gold Medal
-//Ternary Replacement
-//Preliminary Number1
-5 >= 3 ? $Result = "is greater than" : $Result = "is not greater than";
+//--------------------------Gold Medal (Extra Credit)----------------------------
+//-----Number1 (Replaces above if statements with ternary's.)
+
+//--Preliminary Number1
+5 >= 3 ? ($Result = "is greater than") : ($Result = "is not greater than");
 console.log($Result);
 
-//Preliminary Number2
-"cat" === 3 ? $Result = "is the length" : $Result = "is not the length";
-console.log($Result);
-//The if-statement above reacts differently when in ternary format. Is that correct?
-
-//Preliminary Number3
-"cat" === "dog" ? $Result = "the same": $Result = "not the same";
+//--Preliminary Number2
+//This if-statement reacts differently when in ternary format. Is that correct?
+"cat" === 3 ? ($Result = "is the length") : ($Result = "is not the length");
 console.log($Result);
 
-//Bronze Number1
+//--Preliminary Number3
+"cat" === "dog" ? ($Result = "the same") : ($Result = "not the same");
+console.log($Result);
+
+//--Bronze Number1
 var person = { name: "Bobby", age: 12 };
-"Bobby".age === 18 ? $Result = "allowed" : $Result = "underage";
+"Bobby".age === 18 ? ($Result = "allowed") : ($Result = "underage");
 console.log($Result);
+
+//--Bronze Number2
+"Bobby".charAt(0) === "B" ? ($Result = "allowed") : ($Result = "Invalid");
+console.log($Result);
+
+//--Bronze Number3
+"Bobby".charAt(0) === "B" && "Bobby".age === 18 ? ($Result = "allowed") : ($Result = "not allowed");
+console.log($Result);
+
+//--Silver Number1
+1 === 1 || 1 == "1" ? $Result = "strict" || "loose" : $Result = "not equal at all";
+console.log($Result);
+
+//--Silver Number2
+1 <= 2 && 2 === 4 ? $Result = "yes" : $Result = "no";
+console.log($Result);
+
+//-----Number2 (Write an if statement that checks to see if "dog" is a string.)
