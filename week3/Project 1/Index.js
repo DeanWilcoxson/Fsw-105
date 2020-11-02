971452c5953dc1e5936a777c008f3365258ea5e3
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-01 23:24:25 -0800

    11/1/20

---
+++ week3/Project 1/Index.js
@@ -0,0 +1,33 @@
+var peopleWhoWantToSeeMadMaxFuryRoad = [{
+        name: "Mike",
+        age: 12,
+        gender: "Male"
+    },
+    {
+        name: "Madeline",
+        age: 80,
+        gender: "Female"
+    },
+    {
+        name: "Cheryl",
+        age: 22,
+        gender: "Female"
+    },
+    {
+        name: "Sam",
+        age: 30,
+        gender: "Male"
+    },
+    {
+        name: "Suzy",
+        age: 4,
+        gender: "Female"
+    }
+];
+for (var i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++) {
+    if (peopleWhoWantToSeeMadMaxFuryRoad.length.age >= 18) {
+        console.log("Old Enough");
+    } else {
+        console.log("Not Old Enough");
+    }
+}
