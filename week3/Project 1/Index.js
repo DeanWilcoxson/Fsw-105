9f9f2a6a68528bf0890d517fb3aadc309ed90735
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-05 21:22:38 -0800

    11/5/20

--- week3/Project 1/Index.js
+++ week3/Project 1/Index.js
@@ -52,14 +52,17 @@ for (i = 0; i < peopleWhoWantToSeeMadMaxFuryRoad.length; i++) {
 console.log("=======Number 4=======");
 /*#4 Create a for loop that iterates through 101 numbers (from 0 - 100). 
 If the current iteration is an Odd number, print "Odd" to the console, otherwise print "Even".*/
-var arrayOfNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
-for (i = 0; i < arrayOfNumbers.length; i++) {
-    if (arrayOfNumbers[i] % 2 == 1) {
-        console.log("Odd");
-    } else if (arrayOfNumbers[i] % 2 == 0) {
-        console.log("Even");
+var count = 0;
+for (var i = 0; i < 101; i++) {
+    if (count % 2 == 0) {
+        console.log(count + " is even.");
+        count++;
+    } else if (count % 2 !== 0) {
+        console.log(count + " is odd.");
+        count++;
     }
 }
+
 
 /* # Extra Credit: Imagine you have a button that toggles a light on and off.Loop through the following array of numbers and toggle the button the numbers of times
 for each number.The array[2, 3, 2] would toggle the button 7 times.
