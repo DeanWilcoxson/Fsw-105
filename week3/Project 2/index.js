14c53d196813f60574d26941490dbbcd7daa39b4
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-07 19:52:58 -0800

    11/7/20.1

--- week3/Project 2/index.js
+++ week3/Project 2/index.js
@@ -1,23 +1,28 @@
-//#1 Write a function that accepts two numbers as parameters, and returns the sum.
+//Function 1 - Write a function that accepts two numbers as parameters, and returns the sum.
 function sum(num1, num2) {
     return (num1 + num2);
 }
 var sumResult = sum(100, 100);
 console.log(sumResult);
 
-//Write a function that accepts three numbers as parameters, and returns the largest of those numbers.
+//Function 2 - Write a function that accepts three numbers as parameters, and returns the largest of those numbers.
 function whichNumberIsLargest(num1, num2, num3) {
-    if ((num1 > num2) && (num1 > num3)) {
-        console.log(num1 + " is the Largest Number");
-    } else if ((num2 > num1) && (num2 > num3)) {
-        console.log(num2 + " is the Largest Number");
-    } else if ((num3 > num1) && (num3 > num2)) {
-        console.log(num3 + " is the Largest Number");
-    }
+    return (Math.max(num1, num2, num3));
 }
 var arrayOfNumbers = whichNumberIsLargest(5, 4, 3);
+console.log(arrayOfNumbers);
 
-/*Write a function that accepts one number as a parameter, and returns whether that number is even or odd. 
+/*  if ((num1 > num2) && (num1 > num3)) {
+     console.log(num1 + " is the Largest Number");
+ } else if ((num2 > num1) && (num2 > num3)) {
+     console.log(num2 + " is the Largest Number");
+ } else if ((num3 > num1) && (num3 > num2)) {
+     console.log(num3 + " is the Largest Number");
+ } else {
+     console.log(num1 + ", " + num2 + ", or " + num3 + " is Not a Number");
+ } */
+
+/*Function 3 - Write a function that accepts one number as a parameter, and returns whether that number is even or odd. 
 (Return the string "even" or "odd");*/
 function oddOrEven(num1) {
     if (num1 % 2 === 1) {
@@ -28,19 +33,21 @@ function oddOrEven(num1) {
 }
 var myFavoriteNumber = oddOrEven(10);
 
-/*Write a function that accepts a string as a parameter. 
+/*Function 4 - Write a function that accepts a string as a parameter. 
 If the length of the string is less than or equal to twenty characters long, return the string concatenated with itself (string + string). 
 If the string is more than twenty characters long, return the first half of the string.*/
-/* function stringParameter(string) {
-    var x;
-    if (string.length < 20) {
-        x = Math.floor(string.length + string.length);
-    } else {
-        x = Math.floor(string.length / 2) - 1;
+function stringFunction(string) {
+    if (string.length <= 20) {
+        console.log(string + " " + string);
+    } else if (string.length > 20) {
+        console.log(string.slice(0, string.length / 2));
     }
-    return string.substring(0, x);
 }
+var randomPhrase = stringFunction("I hope I get it!");
+/*Extra Credit Function 1 - Write a function that accepts a number‘ n’ as a parameter.Then print the first‘ n’ Fibonacci numbers and
+return their sum.   The first numbers are: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+So, if n were 6, the sum of 1 + 1 + 2 + 3 + 5 + 8 would be 20. (Don't hardcode the sequence!)*/
 
-var half = stringParameter("HowdyDoodyGalsAndFellas");
 
-console.log(half); */
+/*Extra Credit Function 2 - Write a function that accepts a string as a parameter.
+Return the most frequently occurring letter in that string.(White spaces count as a letter)*/
