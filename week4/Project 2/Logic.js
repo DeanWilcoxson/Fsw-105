d000a7ff610c854ad19212af45b8696717d804d8
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-12 01:39:53 -0800

    11/12/20

---
+++ week4/Project 2/Logic.js
@@ -0,0 +1,28 @@
+const readline = require("readline-sync");
+
+function add(num1, num2) {
+    return (num1 + num2);
+}
+var addResult = add(100, 100);
+console.log(addResult);
+
+
+function subtract(num1, num2) {
+    return (num1 - num2);
+}
+var subtractResult = subtract(100, 100);
+console.log(subtractResult);
+
+
+function multiply(num1, num2) {
+    return (num1 * num2);
+}
+var multiplyResult = multiply(100, 100);
+console.log(multiplyResult);
+
+
+function divide(num1, num2) {
+    return (num1 / num2);
+}
+var divideResult = divide(100, 100);
+console.log(divideResult);
