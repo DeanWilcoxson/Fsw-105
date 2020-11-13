75300425997dbf189896a1f33f7a3795793b34a6
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-12 23:37:25 -0800

    11/12/20.6

--- week4/Project 1/2-ArrayMethods.js
+++ week4/Project 1/2-ArrayMethods.js
@@ -2,65 +2,63 @@
 var yummyFruits = ["banana", "apple", "orange", "watermelon"];
 var yummyVeggies = ["carrot", "tomato", "pepper", "lettuce"];
 
-function foodFunction() {
-    //Number 1 - Remove the last item from the vegetable array.
-    console.log("======Number 1======");
 
-    var veggieArr = yummyVeggies.pop();
-    console.log(yummyVeggies);
+//Number 1 - Remove the last item from the vegetable array.
+console.log("======Number 1======");
 
-    //Number 2 - Remove the first item from the fruit array.
-    console.log("======Number 2======");
+var veggieArr = yummyVeggies.pop();
+console.log(yummyVeggies);
 
-    var fruitsArr = yummyFruits.shift();
-    console.log(yummyFruits);
+//Number 2 - Remove the first item from the fruit array.
+console.log("======Number 2======");
 
-    //Number 3 - Find the index of "orange."
-    console.log("======Number 3======");
+var fruitsArr = yummyFruits.shift();
+console.log(yummyFruits);
 
-    var orangeIndex = yummyFruits.indexOf("orange");
-    console.log(orangeIndex);
+//Number 3 - Find the index of "orange."
+console.log("======Number 3======");
 
-    //Number 4 - Add that number to the end of the fruit array.
-    console.log("======Number 4======");
+var orangeIndex = yummyFruits.indexOf("orange");
+console.log(orangeIndex);
 
-    var yummyFruitsPlusOne = yummyFruits.push(orangeIndex);
-    console.log(yummyFruits);
+//Number 4 - Add that number to the end of the fruit array.
+console.log("======Number 4======");
 
-    //Number 5 - Use the length property to find the length of the vegetable array.
-    console.log("======Number 5======");
+var yummyFruitsPlusOne = yummyFruits.push(orangeIndex);
+console.log(yummyFruits);
 
-    var yummyVeggiesLength = yummyVeggies.length;
-    console.log(yummyVeggiesLength);
+//Number 5 - Use the length property to find the length of the vegetable array.
+console.log("======Number 5======");
 
-    //Number 6 - Add that number to the end of the vegetable array.
-    console.log("======Number 6======");
+var yummyVeggiesLength = yummyVeggies.length;
+console.log(yummyVeggiesLength);
 
-    var yummyVeggiesPlusOne = yummyVeggies.push(yummyVeggiesLength);
-    console.log(yummyVeggies);
+//Number 6 - Add that number to the end of the vegetable array.
+console.log("======Number 6======");
 
-    //Number 7 - Put the two arrays together into one array.Fruit first.Call the new Array "food".
-    console.log("======Number 7======");
+var yummyVeggiesPlusOne = yummyVeggies.push(yummyVeggiesLength);
+console.log(yummyVeggies);
 
-    var food = yummyFruits.concat(yummyVeggies);
-    console.log(food);
+//Number 7 - Put the two arrays together into one array.Fruit first.Call the new Array "food".
+console.log("======Number 7======");
 
-    //Number 8 - Remove 2 elements from your new array starting at index 4 with one method.
-    console.log("======Number 8======");
+var food = yummyFruits.concat(yummyVeggies);
+console.log(food);
 
-    var newFoodsArr = food.splice(4, 2);
-    console.log(food);
+//Number 8 - Remove 2 elements from your new array starting at index 4 with one method.
+console.log("======Number 8======");
 
-    //Number 9 - Reverse your array.
-    console.log("======Number 9======");
+var newFoodsArr = food.splice(4, 2);
+console.log(food);
 
-    var reversedFood = food.reverse();
-    console.log(reversedFood);
+//Number 9 - Reverse your array.
+console.log("======Number 9======");
 
-    //Number 10 - Turn the array into a string and return it.
-    console.log("======Number10======");
+var reversedFood = food.reverse();
+console.log(reversedFood);
 
-    var foodString = reversedFood.join();
-    console.log(foodString);
-}
-console.log(foodFunction());
+//Number 10 - Turn the array into a string and return it.
+console.log("======Number10======");
+
+var foodString = reversedFood.join();
+console.log(foodString);
