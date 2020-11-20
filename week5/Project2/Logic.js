d63b25e433e5f4e61d47eb68df3d8526804c85d0
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-20 15:29:54 -0800

    11/20/20.3

--- week5/Project2/Logic.js
+++ week5/Project2/Logic.js
@@ -2,13 +2,13 @@
 /*1) Replace all the vars with let and const. Alter the code however necessary to make sure this continues to work (so the pet's name isn't "John", but instead "spot" is returned). 
 You only need to delete var and insert let and const. John is the pet owner, and his name should be stored differently than the other names.*/
 console.log("=====Number1=====");
-var name = "John";
-var age = 101;
+const manName = "John";
+const age = 101;
 
 function runForLoops(pets) {
-    var petObjects = [];
-    for (var i = 0; i < pets.length; i++) {
-        var pet = { type: pets[i] };
+    const petObjects = [];
+    for (let i = 0; i < pets.length; i++) {
+        let pet = { type: pets[i] };
         if (pets[i] === "cat") {
             name = "fluffy";
         } else { name = "spot"; }
@@ -16,7 +16,7 @@ function runForLoops(pets) {
         pet.name = name;
         petObjects.push(pet);
     }
-    console.log("Man Name: ", name);
+    console.log("Man Name: ", manName);
     return petObjects;
 }
 runForLoops(["cat", "dog"]);
@@ -56,15 +56,39 @@ const people = [{
 /*4) Re-write the following functions to be arrow functions:*/
 console.log("=====Number4=====");
 
+function doMathSum(a, b) {
+    return a + b;
+}
+var produceProduct = function(a, b) {
+    return a * b;
+};
 /*5) Write a printString function that takes firstName, lastName, and age as parameters and returns a string like the following:
 firstName should default to "Jane", lastName should default to "Doe", age should default to 100*/
 console.log("=====Number5=====");
 
+function printString(fName, lName, age) {
+
+}
 /*6) Use template literals to build the string from Requirement 5*/
 console.log("=====Number6=====");
 
 /*7) Use the shorthand syntax to make the following filter take up one line. Copy and paste the array to test it.*/
 console.log("=====Number7=====");
+const animals = [
+    { type: "dog", name: "theodore" },
+    { type: "cat", name: "whiskers" },
+    { type: "pig", name: "piglette" },
+    { type: "dog", name: "sparky" }
+];
 
+function filterForDogs(arr) {
+    return arr.filter(animal => {
+        if (animal.type === "dog") {
+            return true;
+        } else {
+            return false;
+        }
+    });
+}
 /*8) Using template literals, write a function that takes location and name parameters and outputs a message formatted like this:*/
 console.log("=====Number8=====");
