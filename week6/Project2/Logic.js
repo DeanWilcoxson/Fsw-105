2a40ab1c5d4e2966e7cc6b5caa520c2df303411f
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-25 11:13:00 -0800

    11/25/20

--- week6/Project2/Logic.js
+++ week6/Project2/Logic.js
@@ -1,50 +1,18 @@
 /* jshint esversion: 6 */
-const employees = [{
-        name: "John Wayne",
-        jobTitle: "Gunslinger",
-        salary: "$10,000",
-        status: "Full-time"
-    },
-    {
-        name: "John Deer",
-        jobTitle: "Engineer",
-        salary: "$5,000",
-        status: "Full-time"
-    },
-    {
-        name: "Dean Martin",
-        jobTitle: "Entertainer",
-        salary: "$20,000",
-        status: "Part-Time"
-    },
-    {
-        name: "Darth Vader",
-        jobTitle: "Sith Lord",
-        salary: "$100,000",
-        status: "Full-time"
-    },
-    {
-        name: "Clark Kent",
-        jobTitle: "Reporter",
-        salary: "$1,000",
-        status: "Part-time"
-    },
-    {
-        name: "Superman",
-        jobTitle: "Hero",
-        salary: "$0",
-        status: "Full-time"
-    },
-    {
-        name: "Bruce Wayne",
-        jobTitle: "Billionaire, Genius",
-        salary: "$1,000,000,000",
-        status: "Per-Diem"
-    },
-    {
-        name: "Batman",
-        jobTitle: "Hero",
-        salary: "$0",
-        status: "Full-Time"
-    }
-];
+const employees = [];
+
+function Employee(name, jobTitle, salary, status) {
+    this.name = name;
+    this.jobTitle = jobTitle;
+    this.salary = salary;
+    this.status = status;
+}
+Employee.prototype.hireOnEmployee = function() {
+    this.status = "Part-time";
+};
+let theBoss = new Employee("Darth Vader", "Corrupted Jedi", "$2,000", "Full-time");
+let theAssistant = new Employee("John Wayne", "Gunslinger", "$1,000", "Full-time");
+let theGrunt = new Employee("The Hulk", "Avenger-For-Hire", "$5,000", "Contract");
+theGrunt.hireOnEmployee();
+employees.push(theBoss, theAssistant, theGrunt);
+console.log(employees);
