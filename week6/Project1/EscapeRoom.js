d40659c7ea264fa898b4df53bdc4d725db0b41ca
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-24 21:08:45 -0800

    11/24/20

--- week6/Project1/EscapeRoom.js
+++ week6/Project1/EscapeRoom.js
@@ -5,32 +5,22 @@ let lifePoint = true;
 let gotKey = false;
 console.log("\n \t What kind of a name is " + name1 + "?? Hahahah");
 while (lifePoint == true) {
-
-    //Choices
     const choices = readline.keyIn(`Hahaha Which do you prefer TO BE YOUR END ${name1}!? 
         \n \t You Have but three options:
         \n 1.Try your luck in the pit. But Beware, Unknown dangers lurk there, where no light resides. 
         \n 2.Try to open a door. You will need a key for that genius! Good luck finding one!!!
         \n 3.Look for a key. However, I'll never let you escape! I don't get amusement like this very often you Worm!`, { limit: `$<1-3>` });
-
-    //Choice 1
     if (choices == 1) {
         console.log(`\n \t Hahaha ${name1} You Fool! The pit will consume you, just like it did to ALL the others before you!`);
         lifePoint = false;
         console.log("\n \t Game Over");
-    }
-
-    //Choice 2 ( without key / with key )    
-    else if (choices == 2 && gotKey == false) {
+    } else if (choices == 2 && gotKey == false) {
         console.log(`\n \t Hahaha ${name1} You Fool! You need a key to unlock a door. Good luck finding it!!!`);
     } else if (choices == 2 && gotKey == true) {
         console.log(`\n \t WHAT! You Unlocked the door?! You actually found a key? In this needle and a Hay stack situation?? Impossible Noo!!!!
         \n Thanks for playing!!! It was fun!`);
         lifePoint = false;
-    }
-
-    //Choices 3 ( without key / with key )    
-    else if (choices == 3 && gotKey == false) {
+    } else if (choices == 3 && gotKey == false) {
         console.log("\n \t You Found a Key!!");
         console.log(`\n \t Hey! What are you doing over there, stop looking through my stuff!! You'll Never find that key. I'm a Slob for a reason Hahahaha!`);
         gotKey = true;
---
+++ week6/Project2/Logic.js
@@ -0,0 +1,50 @@
+/* jshint esversion: 6 */
+const employees = [{
+        name: "John Wayne",
+        jobTitle: "Gunslinger",
+        salary: "$10,000",
+        status: "Full-time"
+    },
+    {
+        name: "John Deer",
+        jobTitle: "Engineer",
+        salary: "$5,000",
+        status: "Full-time"
+    },
+    {
+        name: "Dean Martin",
+        jobTitle: "Entertainer",
+        salary: "$20,000",
+        status: "Part-Time"
+    },
+    {
+        name: "Darth Vader",
+        jobTitle: "Sith Lord",
+        salary: "$100,000",
+        status: "Full-time"
+    },
+    {
+        name: "Clark Kent",
+        jobTitle: "Reporter",
+        salary: "$1,000",
+        status: "Part-time"
+    },
+    {
+        name: "Superman",
+        jobTitle: "Hero",
+        salary: "$0",
+        status: "Full-time"
+    },
+    {
+        name: "Bruce Wayne",
+        jobTitle: "Billionaire, Genius",
+        salary: "$1,000,000,000",
+        status: "Per-Diem"
+    },
+    {
+        name: "Batman",
+        jobTitle: "Hero",
+        salary: "$0",
+        status: "Full-Time"
+    }
+];
