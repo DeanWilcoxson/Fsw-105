b5000c6f7a59bd9c81056a310b110f468cd6a062
DeanWilcoxson <dean.wilcoxson@bryanuniversity.edu>
2020-11-24 23:28:07 -0800

    11/24/20

--- week6/Project1/EscapeRoom.js
+++ week6/Project1/EscapeRoom.js
@@ -1,9 +1,9 @@
 /* jshint esversion: 6 */
 const readline = require("readline-sync");
-const name1 = readline.question("\n I am the Entity, What is your name unfortunate one?: ");
+const name1 = readline.question(`\n I am the Entity, What is your name unfortunate one?: `);
 let lifePoint = true;
 let gotKey = false;
-console.log("\n \t What kind of a name is " + name1 + "?? Hahahah");
+console.log(`\n \t What kind of a name is " + name1 + "?? Hahahah`);
 while (lifePoint == true) {
     const choices = readline.keyIn(`Hahaha Which do you prefer TO BE YOUR END ${name1}!? 
         \n \t You Have but three options:
@@ -13,7 +13,7 @@ while (lifePoint == true) {
     if (choices == 1) {
         console.log(`\n \t Hahaha ${name1} You Fool! The pit will consume you, just like it did to ALL the others before you!`);
         lifePoint = false;
-        console.log("\n \t Game Over");
+        console.log(`\n \t Game Over`);
     } else if (choices == 2 && gotKey == false) {
         console.log(`\n \t Hahaha ${name1} You Fool! You need a key to unlock a door. Good luck finding it!!!`);
     } else if (choices == 2 && gotKey == true) {
@@ -21,10 +21,10 @@ while (lifePoint == true) {
         \n Thanks for playing!!! It was fun!`);
         lifePoint = false;
     } else if (choices == 3 && gotKey == false) {
-        console.log("\n \t You Found a Key!!");
+        console.log(`\n \t You Found a Key!!`);
         console.log(`\n \t Hey! What are you doing over there, stop looking through my stuff!! You'll Never find that key. I'm a Slob for a reason Hahahaha!`);
         gotKey = true;
     } else if (choices == 3 && gotKey == true) {
-        console.log("\n \t Whats that in your hand? Give me that back!");
+        console.log(`\n \t Whats that in your hand? Give me that back!`);
     }
 }
